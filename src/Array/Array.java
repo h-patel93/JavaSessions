6feeebfeb91b12a77961068b6947c4479a59809f
h-patel93 <45971941+h-patel93@users.noreply.github.com>
2020-07-31 12:02:07 -0400

    Initial commit

---
+++ src/Array/Array.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args){
+
+    int a[] = new int[5];
+
+
+    a[0]=10;
+    a[1]=11;
+    a[2]=12;
+    a[3]=13;
+    a[4]=14;
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+        for(int i = 0;i<5;i++)
+    {
+        System.out.println(a[i]);
+
+    }
+}}

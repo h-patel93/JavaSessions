cfaa5280d5c0bb8352dc80e5233f8cc32ff52e63
h-patel93 <45971941+h-patel93@users.noreply.github.com>
2020-07-31 12:03:54 -0400

    Array Practice-1

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -3,7 +3,7 @@ package Array;
 public class Array {
     public static void main(String[] args){
 
-    int a[] = new int[5];
+    int a[] = new int[6];
 
 
     a[0]=10;
@@ -11,6 +11,7 @@ public class Array {
     a[2]=12;
     a[3]=13;
     a[4]=14;
+    a[5]=15;
 
         System.out.println(a[0]);
         System.out.println(a[1]);
@@ -18,7 +19,7 @@ public class Array {
         System.out.println(a[3]);
         System.out.println(a[4]);
 
-        for(int i = 0;i<5;i++)
+        for(int i = 0;i<6;i++)
     {
         System.out.println(a[i]);
 
